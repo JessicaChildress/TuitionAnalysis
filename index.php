23f3b307b35411287fcd212e9905e965fe071cf1
JessicaChildress <jchildress@zagmail.gonzaga.edu>
2023-12-19 11:21:27 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Graduate Enrollment Management</title>
+</head>
+<body>
+    <h1>Tuition Analysis</h1>
+    <ul>
+        <li><a href="tuition_analysis_home.php">Run New Tuition Analysis</a></li>
+        <li><a href="add_program.php">Add Program</a></li>
+        <li><a href="edit_program.php">Edit Program</a></li>
+        <li><a href="remove_program.php">Remove Program</a></li>
+    </ul>
+</body>
+</html>
