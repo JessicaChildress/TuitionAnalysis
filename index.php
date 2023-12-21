68ffe6fc3b585d854070c217011d82d4dc2e75c2
JessicaChildress <jchildress@zagmail.gonzaga.edu>
2023-12-21 11:36:54 -0800

    not sure what is going on

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
     <h1>Tuition Analysis</h1>
     <ul>
         <li><a href="tuition_analysis_home.php">Run New Tuition Analysis</a></li>
+        <li><a href="view_programs.php">View Programs</a></li>
         <li><a href="add_program.php">Add Program</a></li>
         <li><a href="edit_program.php">Edit Program</a></li>
         <li><a href="remove_program.php">Remove Program</a></li>
+        <li><a href="guzzle_requests.php">Guzzle Requests</a></li>
     </ul>
 </body>
 </html>
